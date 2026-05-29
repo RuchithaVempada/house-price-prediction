184437a39dc9de9a8b6805208e44152f4270b371
Vempada Ruchitha <vempadaruchithareddy@gmail.com>
2026-05-29 20:53:00 +0530

    Connected live backend

--- frontend/src/pages/Prediction.js
+++ frontend/src/pages/Prediction.js
@@ -121,7 +121,7 @@ function Prediction() {
       const response =
         await axios.post(
 
-          "http://127.0.0.1:5000/predict",
+          "https://dreamhouse-backend-o3e2.onrender.com/predict",
 
           {
 
